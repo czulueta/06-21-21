94481a2471d16335efa2f96f0f53927f5bf4809e
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-06-22 21:18:58 -0700

    built basic server component with models folder and and todo component along with routes folder and todo router component

---
+++ routes/todoRouter.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const todoRouter = express.Router()
+const Todo = require("../models/todo.js")
+
+todoRouter.get("/", (req, res, next) => {
+    Todo.find((err, todos) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(todos)
+    })
+})
+todoRouter.post("/", (req, res, next) => {
+    const newTodo = new Todo(req.body)
+    newTodo.save((err, savedTodo) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send()
+    })
+})
